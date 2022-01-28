6f7c62382fc595fa062f92b41473b4bfd2203fcf
Zack Jung <ckse93@gmail.com>
2022-01-28 09:44:20 -0600

    first project setup

---
+++ tabViewWithChild/tabViewWithChild/tabViewWithChildApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabViewWithChildApp.swift
+//  tabViewWithChild
+//
+//  Created by Zack Jung on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct tabViewWithChildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
